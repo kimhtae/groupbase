0851abd21726fb1a4e46987842801dc446650c2e
Kim <andrewk@ablgam.com>
2019-01-23 14:21:21 +0900

    django version 2.1.5 Login Rebuilding

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+#from django.contrib.auth import views as auth_views
+#from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from . import views 
 
+
+
 urlpatterns = [
-	path('login/', auth_views.login, name='login', kwargs={
-		'template_name': 'accounts/login_form.html',
-	}),
-	path('logout/', auth_views.logout, name='logout', kwargs={
+	path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+	
+	path('logout/', LogoutView.as_view(), name='logout', kwargs={
 		'next_page': '/',
 	}),
 	path('signup/', views.signup, name='signup'),
@@ -20,9 +22,9 @@ urlpatterns = [
 	# path('password_change/done/', auth_views.password_change_done),
 	
 	#path('password_reset/', views.reset_password, name='password_reset'),
-	path('reset_password/', password_reset, name='password_reset'),
-	path('reset_password/done', password_reset_done, name='password_reset_done'),
-	url('reset_password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, name='password_reset_confirm'),
-	path('reset_password/complete/', password_reset_complete, name='password_reset_complete'),
+	path('reset_password/', PasswordResetView.as_view(), name='password_reset'),
+	path('reset_password/done', PasswordResetDoneView.as_view(), name='password_reset_done'),
+	url('reset_password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+	path('reset_password/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
